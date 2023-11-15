ad6c28bcecf7b53acb0198f08b1a93011076855b
haizeaF <haizea.franco@gmail.com>
2023-11-15 20:38:56 +0100

    dataframe con idiomas de tweets por usuario

--- dataCleaner.py
+++ dataCleaner.py
@@ -1,10 +1,13 @@
 import pandas as pd
-
-dropColumns = ['created_at', 'id', 'id_str', 'text', 'source', 'truncated', 'in_reply_to_status_id', 'in_reply_to_status_id_str',
-    'in_reply_to_user_id', 'in_reply_to_screen_name', 'geo', 'coordinates', 'place', 'contributors', 'retweet_count', 'favorited',
-    'retweeted', 'possibly_sensitive', 'lang']
+import numpy as np
 
 # Sustituir la lectura del json por archivo obtenido del Streaming
 df = pd.read_json('./database/tweets.json')
 
-df.drop(columns=dropColumns, inplace=True)
+df['user_id'] = df['user'].apply(lambda x: x.get('id_str') if isinstance(x, dict) else np.nan)
+usersDf = df.groupby('user_id').agg({
+    'lang': lambda x: list(set(x)),
+    'user': 'first',
+}).reset_index()
+
+print(usersDf)
