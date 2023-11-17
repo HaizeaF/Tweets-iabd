f7401537586572a3d86887f74bf2b9fe7559f172
HaizeaF <haizea.franco@gmail.com>
2023-11-17 13:39:59 +0100

    count no funcional

--- dataCleaner.py
+++ dataCleaner.py
@@ -5,9 +5,13 @@ import numpy as np
 df = pd.read_json('./database/tweets.json')
 
 df['user_id'] = df['user'].apply(lambda x: x.get('id_str') if isinstance(x, dict) else np.nan)
-usersDf = df.groupby('user_id').agg({
-    'lang': lambda x: list(set(x)),
-    'user': 'first',
-}).reset_index()
+df['mentions_count'] = df['entities'].apply(lambda x: len(x.get('user_mentions')) if isinstance(x, dict) and x.get('user_mentions') else 0)
 
-print(usersDf)
+usersDf = df.groupby('user_id').agg({
+    'user': 'first',
+    'lang': lambda x: list(set(x)),
+    'mentions_count': lambda x: sum(x > 0)
+}).reset_index()
+usersDf['mentions_count'] = df.groupby('user_id')['mentions_count'].sum().reset_index()['mentions_count']
+
+print(usersDf.tail)
