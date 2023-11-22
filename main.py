be19b68701bbd172abd77390553f5705a937c312
haizeaf <haizea.franco@gmail.com>
2023-11-22 20:55:57 +0100

    subida a mongo funcional

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from database.dbContext import *
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, BooleanType
+from pyspark.sql.functions import col, struct
 from hdfs import InsecureClient
-import pandas as pd
 import json
 
 # Pyspark
@@ -13,9 +13,6 @@ def pySpark():
             SparkSession.builder
             .master('local[*]')
             .appName('leerDatos')
-            # .config("spark.mongodb.input.uri", "mongodb://192.168.22.134:27017/tweetsRetoDb.tweets2")
-            # .config("spark.mongodb.output.uri", "mongodb://192.168.22.134:27017/tweetsRetoDb.tweets2")
-            # .config("spark.jars.packages", "org.mongodb.spark:mongo-spark-connector_2.12:3.0.1")
             .getOrCreate()
         )
         
@@ -29,15 +26,15 @@ def pySpark():
                 StructField("location", StringType(), True),
                 StructField("url", StringType(), True),
                 StructField("description", StringType(), True),
-                StructField("protected", StringType(), True),
-                StructField("followers_count", StringType(), True),
-                StructField("friends_count", StringType(), True),
+                StructField("protected", BooleanType(), True),
+                StructField("followers_count", IntegerType(), True),
+                StructField("friends_count", IntegerType(), True),
                 StructField("created_at", StringType(), True),
-                StructField("verified", StringType(), True),
-                StructField("statuses_count", StringType(), True),
+                StructField("verified", BooleanType(), True),
+                StructField("statuses_count", IntegerType(), True),
                 StructField("lang", StringType(), True),
-                StructField("default_profile", StringType(), True),
-                StructField("default_profile_image", StringType(), True)
+                StructField("default_profile", BooleanType(), True),
+                StructField("default_profile_image", BooleanType(), True)
             ]), True),
             StructField("entities", StructType([
                     StructField("user_mentions", StructType([
@@ -48,69 +45,17 @@ def pySpark():
         
         # Crear un DataFrame leyendolos datos en streaming con el esquema y directorio definido
         DF = (
-            spark.readStream
+            spark.read
             .format('json')
             .schema(schema)
             .load('database/dataset/input')
         )
-        
-        # # Crear un DataFrame que desde la conexión a localhost:9999
-        # DF = (
-        #     spark.readStream
-        #     .format('socket')
-        #     .option('host', 'localhost')
-        #     .option('port', 9999)
-        #     .load()
-        # )
-        
-        # Mostrar datos por consola
-        query = DF.writeStream.format('console').start()
-        query.awaitTermination(5)
-        
-        query = DF.writeStream.foreachBatch(
-            lambda batch_df, batch_id: write_row_to_mongo(batch_df)).start() # spark # descomentar si voy a recivir de la base de datos MongoDB
-        query.awaitTermination()
+        structuredDF = DF.withColumnRenamed("lang","tweet_lang").select("tweet_lang","user.*","entities.*")
         
         # Devolver el DataFrame
-        return DF
-    except Exception as error:
-        logging.error(error)
-
-def write_row_to_mongo(batch_df):
-    try:
-        print("\n\n\n\n\n", batch_df.toJSON().collect())
-        print("\n\n\n\n\n", json.dumps(batch_df.toJSON().collect()))
-        # print("\n\n\n\n\n", batch_df.toJSON().map(lambda j: json.loads(j)).collect())
-        
-        
-        context = dbContext()
-        context.importFile(json.dumps(batch_df.toJSON().collect()), 'tweetsRetoDb', 'tweets2')
-        
-        # df_tweets = batch_df
-        # df_tweets = df_tweets.filter(df_tweets.lang.isNotNull()) # Limpiar tweets vacios
-        # df_tweets.write.format('mongo').mode('append').save()
-    except Exception as error:
-        logging.error(error)
-
-def exportarDatos(DF):
-    try:
-        # DF.toJSON().first()
-        # Ruta de escritura
-        #DF.coalesce(1).writeStream.format("json").option("header", "false").save("database/dataset/output/dataFrame.json")
-
-        # Bueno
-        outputJson = (DF.writeStream
-            .outputMode("append")
-            .format("json")
-            .option("path", "database\dataset\output")
-            .option("checkpointLocation", "database\dataset\output\\basura")
-            .start())
-            
-        # df_spark.writeStream \
-        #     .option('path', 'C:/Users/iabd/Desktop/Grado/Tweets-iabd/database/dataset/output') \
-        #     .option('checkpointLocation', 'database/dataset/temp') \
-        #     .start()
-        
+        pandasDf = structuredDF.toPandas()
+        json_data = pandasDf.to_dict('records')
+        return json.dumps(json_data)
     except Exception as error:
         logging.error(error)
     
@@ -137,7 +82,8 @@ def subirHDFS(DF):
     
 def main():
     DF = pySpark()
-    # exportarDatos(DF)
+    context = dbContext()
+    context.importFile(DF)
     # subirHDFS(DF)
     
 if __name__ == '__main__':
